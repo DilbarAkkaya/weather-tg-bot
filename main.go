e06e9fee4df4ba60f38793caa9e2bed58205d509
DilbarAkkaya <dilbarmutavalova@gmail.com>
2026-05-02 16:46:36 +0300

    feat: add weather api and update go.mod

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
+
+type WeatherResponse struct {
+    Main struct {
+        Temp float64 `json:"temp"`
+    }`json:"main"`
+        Weather []struct {
+            Description string `json:"description"`
+        } `json:"weather"`
+        Name string `json:"name"`
+}
 func main(){
     err:=godotenv.Load()
     if err !=nil {
@@ -25,8 +38,45 @@ func main(){
         if update.Message == nil {
             continue
         }
+        chatId:=update.Message.Chat.ID
+        if update.Message.Location != nil {
+            lat:= update.Message.Location.Latitude
+            lon:= update.Message.Location.Longitude
+            weather, err:= getWeather(lat, lon)
+            if err !=nil{
+               log.Printf("Weather error: %v", err)
+                weather = "I can't getweather, sorry!"
+            }
+            //reply:= fmt.Sprintf("I got your lan %f and lon %f", lat, lon)
+            msg:=tgbotapi.NewMessage(chatId, weather)
+                    bot.Send(msg)
+                    continue
+        }
         log.Printf("[%s]%s", update.Message.From.UserName, update.Message.Text)
-        msg:= tgbotapi.NewMessage(update.Message.Chat.ID, "I hear you! You've written: " + update.Message.Text)
+        btn:= tgbotapi.NewKeyboardButtonLocation("Please send location")
+        keyboard:= tgbotapi.NewReplyKeyboard(tgbotapi.NewKeyboardButtonRow(btn))
+        keyboard.ResizeKeyboard = true
+        msg:=tgbotapi.NewMessage(chatId, "Click button for weather")
+        msg.ReplyMarkup =keyboard
         bot.Send(msg)
     }
+}
+func getWeather(lat, lon float64) (string, error){
+apiKey:= os.Getenv("API_KEY")
+url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s&units=metric", lat, lon, apiKey)
+resp, err:= http.Get(url)
+if err !=nil {
+    return "", err
+}
+defer resp.Body.Close()
+var data WeatherResponse
+err= json.NewDecoder(resp.Body).Decode(&data)
+if err != nil {
+    return "", err
+}
+if len(data.Weather) > 0  {
+description:= data.Weather[0].Description
+return fmt.Sprintf("City: %s Temperature %.1f°C\nSky: %s", data.Name, data.Main.Temp, description), nil
+}
+return "Weather data is empty", nil
 }
